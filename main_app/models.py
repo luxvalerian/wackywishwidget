64a2c9aaa1010bc6182d9f6c4524ca16a576cea2
Avelynn Mitra <avelynnmitra@gmail.com>
2020-05-01 08:46:39 -0500

    add wishes model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Wishes(models.Model):
+    description = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return f"{self.id} - {self.name}, {self.quantity}"
